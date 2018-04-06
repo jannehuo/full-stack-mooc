2d0554603b00f8c019782459eb819c1759371216
Janne Huotari <janne@richen.fi>
2018-04-06 14:22:44 +0300

    osa1: Tehtava 1.3 commit

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -5,13 +5,16 @@ const Otsikko = ({kurssi}) => (
   <h1>{kurssi}</h1>
 )
 
-const Sisalto = ({osaList,tehtaviaList}) => (
-  <div>
-    <Osa osa={osaList[0]} tehtavia={tehtaviaList[0]} />
-    <Osa osa={osaList[1]} tehtavia={tehtaviaList[1]} />
-    <Osa osa={osaList[2]} tehtavia={tehtaviaList[2]} />
-  </div>
-)
+const Sisalto = ({osaList}) => {
+  const osat = osaList.map((osa,i) => {
+    return <Osa key={i} osa={osa.nimi} tehtavia={osa.tehtavia} />
+  })
+  return (
+    <div>
+      {osat}
+    </div>
+  )
+}
 
 const Yhteensa = ({tehtavatYht}) => (
   <p>yhteensä {tehtavatYht} tehtävää</p>
@@ -23,18 +26,24 @@ const Osa = ({osa,tehtavia}) => (
 
 const App = () => {
   const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+  const osa1 = {
+    nimi: 'Reactin perusteet',
+    tehtavia: 10
+  }
+  const osa2 = {
+    nimi: 'Tiedonvälitys propseilla',
+    tehtavia: 7
+  }
+  const osa3 = {
+    nimi: 'Komponenttien tila',
+    tehtavia: 14
+  }
 
   return (
     <div>
       <Otsikko kurssi={kurssi} />
-      <Sisalto osaList={[osa1,osa2,osa3]} tehtaviaList={[tehtavia1,tehtavia2,tehtavia3]} />
-      <Yhteensa tehtavatYht={tehtavia1 + tehtavia2 + tehtavia3}/>
+      <Sisalto osaList={[osa1,osa2,osa3]} />
+      <Yhteensa tehtavatYht={osa1.tehtavia + osa2.tehtavia + osa3.tehtavia}/>
     </div>
   )
 }
