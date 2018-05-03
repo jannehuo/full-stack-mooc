85e64d01e915a7642719e26a0f4f9b0d8a36fb73
Janne Huotari <janne@richen.fi>
2018-05-03 13:32:53 +0300

    tehtavat 1.13 ja 1.14 anekdootit osat 2 ja 3

--- osa1_anekdootit/src/index.js
+++ osa1_anekdootit/src/index.js
@@ -6,28 +6,62 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      selected: 0
+      selected: 0,
+      votes: this.props.anecdotes.map((anecdote,i) => {
+        return 0
+      }),
+      mostVoted:'No Votes Yet.'
     }
     this.showRandomAnectode = this.showRandomAnectode.bind(this);
     this.generateRandomNum = this.generateRandomNum.bind(this);
+    this.voteAnectode = this.voteAnectode.bind(this);
+    this.mostVoted = this.mostVoted.bind(this);
   }
   showRandomAnectode = () => {
     let randomNum = this.generateRandomNum();
     return () => {
       this.setState((prevState) => ({
-        selected: randomNum
+        selected: randomNum,
+        mostVoted:this.mostVoted()
       }));
     }
   }
   generateRandomNum() {
     return Math.floor(Math.random() * this.props.anecdotes.length);
   }
+  voteAnectode = () => {
+    const copy = [...this.state.votes];
+    copy[this.state.selected] += 1;
+    const newState = {
+      votes:copy
+    }
+    return () => {
+      this.setState((prevState) =>(newState));
+    }
+  }
+  mostVoted() {
+    const copy = [...this.state.votes];
+    const valueObj = copy.map((val,i) => {
+      return {
+        index:i,
+        value:val,
+        text:this.props.anecdotes[i]
+      }
+    });
+    const sortedValues = valueObj.sort((a, b) => a.value - b.value).reverse();
+    const firstItem = sortedValues[0];
+    return firstItem.text;
+  }
   render() {
     return (
       <div className='app-container'>
-        <h1>Anekdootit</h1>
+        <h1>Anectodes</h1>
         <p>{this.props.anecdotes[this.state.selected]}</p>
+        <p>{this.state.votes[this.state.selected]} votes.</p>
         <button onClick={this.showRandomAnectode()} className='button neutral'>Next Anectode</button>
+        <button onClick={this.voteAnectode()} className='button positive'>Vote</button>
+        <h1>Anectode with most votes</h1>
+        <p>{this.state.mostVoted}</p>
       </div>
     )
   }
