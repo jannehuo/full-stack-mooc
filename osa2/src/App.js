d0fada745405a6f745a3a57d910d647d58f46a81
Janne Huotari <janne@richen.fi>
2018-05-04 10:30:21 +0300

    osa2 tehtävä 2.5

--- osa2/src/App.js
+++ osa2/src/App.js
@@ -1,31 +1,5 @@
 import React from 'react';
-
-const Kurssit = ({kurssit}) => {
-  const infot = kurssit.map((kurssi) => {
-    return <KurssiInfo key={kurssi.id} kurssi={kurssi} />;
-  });
-  return (
-    <div className='course-info'>
-      {infot}
-    </div>
-  )
-}
-
-const KurssiInfo = ({kurssi}) => {
-  const osat = kurssi.osat.map((osa) => {
-    return <li key={osa.id}>{osa.nimi} {osa.tehtavia}</li>;
-  });
-  const yht = kurssi.osat.reduce((a,b) => {
-    return a + b.tehtavia;
-  },0);
-  return (
-    <div className='course-info-content'>
-      <h1>{kurssi.nimi}</h1>
-      <ul>{osat}</ul>
-      <p className='bold'>Yhteensä: {yht}</p>
-    </div>
-  )
-}
+import Kurssit from './components/Kurssi.js'
 
 const App = () => {
   const kurssit = [{
