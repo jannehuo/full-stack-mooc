57c49562b43e57ddb76c1896720078fbbfe6bb3d
Janne Huotari <janne@richen.fi>
2018-05-04 14:01:11 +0300

    osa2 tehtavat 2.10 ja 2.11

--- osa2/src/App.js
+++ osa2/src/App.js
@@ -1,74 +1,84 @@
-import React from 'react';
-import Persons from './components/Persons.js';
-import Form from './components/Form.js';
-import Filter from './components/Filter.js';
+import React from 'react'
+import Axios from 'axios'
+import Persons from './components/Persons.js'
+import Form from './components/Form.js'
+import Filter from './components/Filter.js'
+
+const appUrls = {
+  persons:'http://localhost:3001/persons/'
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      persons: [
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Martti Tienari', number: '040-123456' },
-        { name: 'Arto Järvinen', number: '040-123456' },
-        { name: 'Lea Kutvonen', number: '040-123456' }
-      ],
+      persons: [],
       newName: '',
       newNumber:'',
       filter:'',
       nameError:false
     }
-    this.submitForm = this.submitForm.bind(this);
-    this.nameInputChange = this.nameInputChange.bind(this);
-    this.phoneInputChange = this.phoneInputChange.bind(this);
-    this.filterInputChange = this.filterInputChange.bind(this);
+    this.submitForm = this.submitForm.bind(this)
+    this.nameInputChange = this.nameInputChange.bind(this)
+    this.phoneInputChange = this.phoneInputChange.bind(this)
+    this.filterInputChange = this.filterInputChange.bind(this)
+  }
+
+  componentWillMount() {
+    Axios.get(appUrls.persons).then((res) => {
+      if(res.status === 200) {
+        this.setState({
+          persons:res.data
+        })
+      }
+    })
   }
 
   submitForm(e) {
-    e.preventDefault();
+    e.preventDefault()
     const newNameObj = {
       name:this.state.newName,
       number:this.state.newNumber
-    };
+    }
     
-    const persons = [...this.state.persons];
-    persons.push(newNameObj);
+    const persons = [...this.state.persons]
+    persons.push(newNameObj)
     if(!this.state.nameError) {
       this.setState({
         persons,
         newName:'',
         newPhone:''
-      });
-      e.target.reset();
+      })
+      e.target.reset()
     }
   }
 
   nameInputChange(e) {
     const alreadyExists = this.state.persons.filter((person) => {
-      return person.name.toLocaleLowerCase() === e.target.value.toLocaleLowerCase();
-    });
+      return person.name.toLocaleLowerCase() === e.target.value.toLocaleLowerCase()
+    })
     if(alreadyExists.length > 0) {
       this.setState({
         nameError: true
-      });
+      })
     } else {
       this.setState({
         newName: e.target.value,
         nameError: false
-      });
+      })
     }
   }
 
   phoneInputChange(e) {
     this.setState({
       newNumber:e.target.value
-    });
+    })
   }
 
   filterInputChange(e) {
     this.setState({
       filter:e.target.value
-    });
+    })
   }
 
   render() {
@@ -84,4 +94,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default App
